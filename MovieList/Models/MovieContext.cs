5545102a3c050de20d1a9c5c687f871f9e8a3003
jp8117 <jp8117@live.com>
2022-10-01 18:57:04 -0400

    Add project files.

---
+++ MovieList/Models/MovieContext.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieList.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options) { }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Genre>().HasData(
+                    new Genre
+                    {
+                        GenreID = "A",
+                        Name = "Action"
+                    },
+                    new Genre
+                    {
+                        GenreID = "C",
+                        Name = "Comedy"
+                    },
+                    new Genre
+                    {
+                        GenreID = "D",
+                        Name = "Drama"
+                    },
+                    new Genre
+                    {
+                        GenreID = "H",
+                        Name = "Horror"
+                    },
+                    new Genre
+                    {
+                        GenreID = "M",
+                        Name = "Musical"
+                    },
+                    new Genre
+                    {
+                        GenreID = "R",
+                        Name = "RomCom"
+                    },
+                    new Genre
+                    {
+                        GenreID = "S",
+                        Name = "SciFi"
+                    }
+                );
+            modelBuilder.Entity<Movie>().HasData(
+                    new Movie
+                    {
+                        MovieId = 1,
+                        Name = "Casablanca",
+                        Year = 1942,
+                        Rating = 5,
+                        GenreID = "D"
+                    },
+                    new Movie
+                    {
+                        MovieId = 2,
+                        Name = "Wonder Woman",
+                        Year = 2017,
+                        Rating = 3,
+                        GenreID = "A"
+                    },
+                    new Movie
+                    {
+                        MovieId = 3,
+                        Name = "Moonstruck",
+                        Year = 1988,
+                        Rating = 5,
+                        GenreID = "R"
+                    }
+                );
+        }
+    }
+}
